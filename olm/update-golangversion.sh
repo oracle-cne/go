8aabdafb1712bcdbd0f232ce408096a4f375c899
Murali Annamneni <murali.annamneni@oracle.com>
2025-08-25 17:52:35 +0000

    Add init script to update golang version for all 0 patch version branches

---
+++ olm/update-golangversion.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TARGET_MINOR=$1
+PATCH_VERSION=$2
+
+if [[ "$PATCH_VERSION" -eq 0 ]]; then
+  exit 0
+fi
+
+find . -name "go.mod" | while read -r file; do
+  if grep -qE "^go 1\.${TARGET_MINOR}(\.[0-9]+)?$" "$file"; then
+    echo "Updating $file"
+    # reduce minor version by 1
+    sed -Ei "s/^go 1\.${TARGET_MINOR}(\.[0-9]+)?$/go 1.$((TARGET_MINOR - 1))/" "$file"
+  fi
+done
